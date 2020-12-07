cc4f3db1d1988e24ed264a17998be8b72b2dd2ac
lx <654382143@qq.com>
2020-12-07 16:01:16 +0800

    DEV分支上3355添加测试类

--- cloud-config-client-3355/src/test/java/com/lx/test/testdev.java
+++ cloud-config-client-3355/src/test/java/com/lx/test/testdev.java
@@ -2,6 +2,8 @@ package com.lx.test;
 
 public class testdev {
     public static void main(String[] args) {
+
+        System.out.println("springboot和springmvc区别");
         System.out.println("测试解决冲突方法");
     }
 }
